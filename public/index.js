5ccad231e7596e1d8e2b04178c910e56c393aea0
George Leyva <leyvageorge@hotmail.com>
2023-02-08 14:56:40 -0800

    changes till part 3

--- public/index.js
+++ public/index.js
@@ -4,6 +4,38 @@ async function main() {
     const highestPriceChartCanvas = document.querySelector('#highest-price-chart');
     const averagePriceChartCanvas = document.querySelector('#average-price-chart');
 
+    let response = await fetch('https://api.twelvedata.com/time_series?symbol=GME,MSFT,DIS,BNTX&interval=1min&apikey=cb3dde24accd4a5392d83f62de00247e')
+    let result = await response.json();
+    console.log(result)
+
+    let GME = result.GME
+    let MSFT = result.MSFT
+    let DIS = result.DIS
+    let BTNX = result.BTNX
+
+    const stocks = [GME, MSFT, DIS, BNTX];
+
+// Bonus Note: 
+// Another way to write the above lines would to refactor it as:
+   // const {GME, MSFT, DIS, BTNX} = result 
+// This is an example of "destructuring" an object
+// "Destructuring" creates new variables from an object or an array
 }
 
+// const timeChartCanvas = document.getElementById('time-chart').getContext('2d')
+// var myChart = new Chart(timeChartCanvas.getContext('2d'), {
+//     type: 'line',
+//     data: {
+//         labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+//         datasets: [{
+//             label: '# of Votes',
+//             data: [12, 19, 3, 5, 2, 3],
+//             backgroundColor: 'rgba(255, 99, 132, 0.2)',
+//             borderColor: 'rgba(255, 99, 132, 1)'
+//         }]
+//     }
+// });
+
+
 main()
+//cb3dde24accd4a5392d83f62de00247e  //KEY for twelvedata.com
